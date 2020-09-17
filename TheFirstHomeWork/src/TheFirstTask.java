7e9c5420007db3fd5138596adab571f227e2c807
Andrei <ncl8@mail.ru>
2020-09-17 18:34:02 +0300

    The first version of home work

---
+++ TheFirstHomeWork/src/TheFirstTask.java
@@ -0,0 +1,6 @@
+public class TheFirstTask {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TheFirstHomeWork/src/TheSecondTask.java
@@ -0,0 +1,15 @@
+public class TheSecondTask {
+
+    public static void main(String[] args) {
+        byte a = 125;
+        short b = 32765;
+        int c = 13;
+        long l = 20000L;
+        float f = 12.45f;
+        double d = -123.123;
+        char charr = 'A';
+        boolean type = true;
+        String name = "Andrei";
+
+    }
+}

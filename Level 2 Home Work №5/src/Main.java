956a07462815c06fce4c8d9790f02d77b883a9e0
Andrei <ncl8@mail.ru>
2020-11-06 21:27:57 +0300

    Синхронизировал потоки расчета и вывода

--- Level 2 Home Work №5/src/Main.java
+++ Level 2 Home Work №5/src/Main.java
@@ -37,22 +37,32 @@ public class Main {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
+        doCalculate(a1, a2);
+        outArr(a1, a2, a, arr);
+
+    }
+
+        public synchronized static void doCalculate(float[] a1, float[] a2){
         Thread t1 = new Thread(() -> {
-                for(int i = 0; i < a1.length; i++){
-                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+            for(int i = 0; i < a1.length; i++){
+                a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
         });
 
         t1.start();
 
         Thread t2 = new Thread(() -> {
-                for(int i = 0; i < a1.length; i++){
-                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-                }
+            for(int i = 0; i < a2.length; i++){
+                a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
         });
 
         t2.start();
 
+
+    }
+
+    public synchronized static void outArr(float[] a1, float[] a2, long a, float [] arr){
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
 

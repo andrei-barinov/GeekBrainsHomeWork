28d173c72a0380431a77723a01c055fddc3fa88b
Andrei <ncl8@mail.ru>
2020-11-08 15:55:18 +0300

    Версия 1.1

--- Level 2 Home Work №5/src/Main.java
+++ Level 2 Home Work №5/src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
-
     static final int size = 10000000;
+    static float[] arr = new float[size];
     static final int h = size / 2;
 
     public static void main(String[] args) {
+
         calculateArray();
         calculateArray_2();
     }
 
     public static void calculateArray(){
-        float[] arr = new float[size];
         for(int i = 0; i < arr.length; i++){
             arr[i] = 1F;
         }
@@ -25,7 +25,6 @@ public class Main {
     }
 
     public static void calculateArray_2(){
-        float[] arr = new float[size];
         float[] a1 = new float[h];
         float[] a2 = new float[h];
         for(int i = 0; i < arr.length; i++){
@@ -37,36 +36,37 @@ public class Main {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        doCalculate(a1, a2);
-        outArr(a1, a2, a, arr);
-
-    }
-
-        public synchronized static void doCalculate(float[] a1, float[] a2){
         Thread t1 = new Thread(() -> {
             for(int i = 0; i < a1.length; i++){
                 a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
 
-        t1.start();
 
         Thread t2 = new Thread(() -> {
-            for(int i = 0; i < a2.length; i++){
+            for(int i = 0; i < a1.length; i++){
                 a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         });
 
+        t1.start();
         t2.start();
 
+        try {
+            t1.join();
+            t2.join();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
 
-    }
-
-    public synchronized static void outArr(float[] a1, float[] a2, long a, float [] arr){
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
 
         System.currentTimeMillis();
         System.out.println(System.currentTimeMillis() - a);
     }
+
 }
+
+
+

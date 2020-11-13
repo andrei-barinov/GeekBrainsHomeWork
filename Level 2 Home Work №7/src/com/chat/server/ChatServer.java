5fdbc0683f64943c138b8f4e385d6eb66148de61
Andrei <ncl8@mail.ru>
2020-11-13 21:40:20 +0300

    Level 2 Home Work №7. Версия 1.0

---
+++ Level 2 Home Work №7/src/com/chat/server/ChatServer.java
@@ -0,0 +1,65 @@
+package com.chat.server;
+
+import com.chat.auth.AuthenticationService;
+import com.chat.auth.BasicAuthenticationService;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatServer implements Server {
+
+    private Set<ClientHandler> clients;
+    private AuthenticationService authenticationService;
+
+    public ChatServer(){
+        try{
+            System.out.println("Сервер запускается...");
+            ServerSocket serverSocket = new ServerSocket(7777);
+            clients = new HashSet<>();
+            authenticationService = new BasicAuthenticationService();
+            System.out.println("Сервер успешно запущен");
+
+            while (true){
+                System.out.println("Сервер ождидает подключения клиента...");
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент успешно подключился. Сокет: " + socket);
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e){
+            throw new RuntimeException("Что-то пошло не так", e);
+        }
+    }
+
+    @Override
+    public void broadcastMessage(String message) {
+        clients.forEach(client -> {
+            client.sendMessage(message);
+        });
+    }
+
+    @Override
+    public synchronized boolean isLoggedIn(String nickname) {
+        return clients.stream()
+                .filter(clientHandler -> clientHandler.getName().equals(nickname))
+                .findFirst()
+                .isPresent();
+    }
+
+    @Override
+    public synchronized void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    @Override
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    @Override
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+}

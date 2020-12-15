efffd97c44c219e6627ac8d8c837c18474387371
Andre <ncl8@mail.ru>
2020-12-15 15:38:11 +0300

    Версия 1.4

---
+++ Level 2 Home Work №7/src/main/java/com/chat/server/Server.java
@@ -0,0 +1,16 @@
+package com.chat.server;
+
+import com.chat.auth.AuthenticationService;
+
+public interface Server {
+    void broadcastMessage(String message);
+    boolean isLoggedIn(String nickname);
+    void subscribe(ClientHandler client);
+    void unsubscribe(ClientHandler client);
+    void sendMessageToClient(String message);
+    //void doFileWriter(String message);
+    //void printHistory (String message, ClientHandler client);
+    void entrIsTrue(String message);
+    void outIsTrue(String message);
+    AuthenticationService getAuthenticationService();
+}
